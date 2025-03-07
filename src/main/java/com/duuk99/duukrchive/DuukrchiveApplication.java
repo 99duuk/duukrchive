b9bf3f5c5eea8aeb922a0ef6532fa077d9a6c37f
dwkim <99duuk@gmail.com>
2025-03-07 19:40:13 +0900

    init

---
+++ src/main/java/com/duuk99/duukrchive/DuukrchiveApplication.java
@@ -0,0 +1,13 @@
+package com.duuk99.duukrchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuukrchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuukrchiveApplication.class, args);
+    }
+
+}
